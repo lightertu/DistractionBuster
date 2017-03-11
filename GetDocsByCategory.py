16f04a58b29f704e82156c08a23dc060e01094a5
lightertu <ruit@uoregon.edu>
2017-03-10 21:49:06 -0800

    ported get docment script to python3

--- GetDocsByCategory.py
+++ GetDocsByCategory.py
@@ -1,38 +1,44 @@
 # Author: Shweta
+# Peer review: The rest
 
-import urllib2
+import urllib
 import csv
 from bs4 import BeautifulSoup
 
-f = open("C:\Users\shwet\Desktop\CS_wiki.csv", 'rb')
-f1 = open("C:\Users\shwet\Desktop\wikiContent.xml", 'a')
+inputFile = open("./Simplex/CompSci.csv", 'r')
+outputFile = open("./Simplex/Dump/CompSci.xml", 'a')
 
-reader = csv.reader(f)
-id=""
-c=0
+reader = csv.reader(inputFile)
+
+pageIds = ""
+counter = 0
 for row in reader:
-	if c==0:
-		c+=1
-		continue
+    if counter==0:
+        counter += 1
+        continue
 
-	id+=row[2]+"|"
+    pageIds += row[2]+"|"
+    counter+=1
 
-	c+=1
-	print id
+    if( counter % 50 == 0 ):
+        url="https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=xml&pageids="+pageIds[:-1]
+        req = urllib.request.urlopen(url)
+        if req.getcode() == 200:
+            soup = BeautifulSoup(req.read(), 'html.parser')
+            s = soup.find_all('page')
 
-	if(c%50==0):
-		url="https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=xml&pageids="+id[:-1]
-		req=urllib2.urlopen(url)
-		if req.getcode() == 200:
-			soup = BeautifulSoup(req.read(), 'html.parser')
-			print len(soup.find_all('page'))
-			print soup.find_all('page')
-			s=soup.find_all('page')
-			for si in s:
-				f1.write(str(si))
-			id=""
+            for si in s:
+                outputFile.write(str(si))
+            pageIds = ""
 
+if pageIds != "":
+    url="https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=xml&pageids="+pageIds[:-1]
+    req = urllib.request.urlopen(url)
+    if req.getcode() == 200:
+        soup = BeautifulSoup(req.read(), 'html.parser')
+        s = soup.find_all('page')
 
-
-
+        for si in s:
+            outputFile.write(str(si))
+        pageIds = ""
 
